3f4cfd27919274827ec91dccbbd84754ae084dfd
TobiasFK <tobias.fabricius@gmail.com>
2022-09-24 14:59:39 -0400

    Complete exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    // checks whether number is negative
+    if (num < 0) {
+        return "ERROR";
+    }
+    let repeated = "";
+    for (i = 1; i <= num; i++) {
+        repeated += string;
+    }
+    return repeated;
 };
 
 // Do not edit below this line
